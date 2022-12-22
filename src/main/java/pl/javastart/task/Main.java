e8e6a0f7792bb7f7f3c2601167a605bc6336b1e4
Krzysiek Przystarz <frodson35@gmail.com>
2022-12-22 18:14:09 +0100

    zadanie 6.2

--- src/main/java/pl/javastart/task/Main.java
+++ src/main/java/pl/javastart/task/Main.java
@@ -1,8 +1,24 @@
 package pl.javastart.task;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Podaj rozmiar tablicy:");
+        int arrays = sc.nextInt();
+        double[] array = new double[arrays];
+        System.out.println("Wprowadź " + arrays + " liczb:");
 
+        for (int i = 0; i < array.length; i++) {
+            array[i] = sc.nextDouble();
+        }
+
+        double sum = 0;
+        for (double x : array) {
+            sum += Math.pow(x, 2);
+        }
+        System.out.println("Suma kwadratów wprowadzonych liczb to: " + sum);
     }
 }
